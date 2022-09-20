7aa55cde176bb303c48b13ef6886202dc54fc417
Lindsey White <LindseyWhite@RocketMortgage.com>
2022-09-19 20:34:48 -0400

    MVP completed

---
+++ Lab1_PersonalWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lab1_PersonalWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
